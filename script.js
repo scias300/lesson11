49e7fbd863c3ebfd88e813cae6d11b9ae31fcd12
Alexander Pashkov <scias300@gmail.com>
2022-06-13 20:28:56 +0300

    выполнил задание урока 11

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+'use strict';
+
+const btn = document.querySelector('#btn');
+const text = document.querySelector('#text');
+const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
+const btn1 = document.querySelector('#e_btn');
+const range = document.querySelector('#range');
+const size = square.clientWidth;
+document.querySelector('#range-span').textContent = range.value;
 
 
+let changeColor = function () {
+    square.style.backgroundColor = text.value;
+};
 
+let changeSize = function () {
+    document.querySelector('#range-span').textContent = range.value;
+    square.style.width = size * range.value / 100 + 'px';
+    square.style.height = size * range.value / 100 + 'px';
+    circle.style.width = size * range.value / 100 + 'px';
+    circle.style.height = size * range.value / 100 + 'px';
+};
 
+btn.addEventListener('click', changeColor);
 
+btn1.style.display = 'none';
+
+range.addEventListener('input', changeSize);
